52990a476c3c164c163b42dcf21261f601bbf380
mlanfe <mlanfe@163.com>
2020-07-11 14:56:24 +0800

    划分项目结构及初始化配置

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'asset': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+  
+}
